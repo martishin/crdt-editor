7b43d05b744ffeecab31f1f1888c8765b2457bb4
Alex <asmartishin@gmail.com>
2024-11-11 00:23:57 +0100

    Update DataEditor.tsx

--- client/src/components/DataEditor.tsx
+++ client/src/components/DataEditor.tsx
@@ -76,7 +76,7 @@ const DataEditor: React.FC = () => {
       </div>
       <footer className="footer">
         <p>
-          Check the CRDT algorithm source code on{" "}
+          Check out the CRDT algorithm code on{" "}
           <a
             href="https://github.com/martishin/react-python-rust-crdt-editor/blob/main/crdt/src/crdt.rs"
             target="_blank"
